5e2ff9dd6db789396d684f39c09f40c7f943f80b
CurryTao <2474047252@qq.com>
2018-10-15 18:58:50 +0800

    会议管理系统

--- src/main/java/com/meeting/m2/MeetingApplication.java
+++ src/main/java/com/meeting/m2/MeetingApplication.java
@@ -1,14 +1,8 @@
 package com.meeting.m2;
 
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.support.config.FastJsonConfig;
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan(value = "com.meeting.m2.dao")
